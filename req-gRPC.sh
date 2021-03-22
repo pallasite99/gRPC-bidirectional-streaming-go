8eb570bf239c737d56673c072eb8f4861d9ce642
Salil Apte <salil.apte99@gmail.com>
2021-03-22 18:52:55 +0530

    Update req-gRPC.sh

--- req-gRPC.sh
+++ req-gRPC.sh
@@ -33,8 +33,8 @@ export PATH="$PATH:$(go env GOPATH)/bin"
 
 # Install protocol compiler plugins for Go
 export GO111MODULE=on  # Enable module mode
-go get google.golang.org/protobuf/cmd/protoc-gen-go \
-         google.golang.org/grpc/cmd/protoc-gen-go-grpc
+go get -u google.golang.org/protobuf/cmd/protoc-gen-go
+go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc
 
 go get -u github.com/golang/protobuf/protoc-gen-go
 
